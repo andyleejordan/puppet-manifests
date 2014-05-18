0200b7f9c52192e1199693880ec13681590f46a7
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2014-05-18 16:10:08 -0700

    Hiera-izing mounts and vcsrepos in nodes

--- nodes.pp
+++ nodes.pp
@@ -1,49 +1,21 @@
 # nodes.pp
 
-node 'vagrant' inherits default {
-}
+node 'vagrant' inherits default {}
 
-node 'krikkit.schwartzmeyer.com' inherits default {
+node 'krikkit.schwartzmeyer.com' inherits default {}
 
-  vcsrepo { '/var/www/idahothegoodlife.com':
-    ensure   => latest,
-    provider => git,
-    source   => 'https://github.com/andschwa/idahothegoodlife.com.git',
-    revision => 'master',
-  }
-}
-
-
-node 'slartibartfast.schwartzmeyer.us' inherits default {
-
-  # shared VBox folder backup
-  $backup_path = '/mnt/backup'
-  $stuff_path  = '/mnt/stuff'
-
-  file { [$backup_path, $stuff_path]:
-    ensure  => directory,
-  }
-
-  Mount {
-    ensure    => mounted,
-    fstype    => 'vboxsf',
-    options   => [ 'rw', 'uid=andrew', 'gid=andrew' ],
-  }
-
-  mount { $backup_path:
-    device    => 'backup',
-    subscribe => File[$backup_path],
-  }
-
-  mount { $stuff_path:
-    device    => 'stuff',
-    subscribe => File[$stuff_path],
-  }
-}
+node 'slartibartfast.schwartzmeyer.us' inherits default {}
 
 node default {
   # hiera ssh keys
   create_resources('ssh_authorized_key', hiera_hash('ssh_keys', {}), hiera_hash('ssh_key_defaults', {}))
+
+  # hiera mounts
+  create_resources('file', hiera_hash('mount_points', {}), {ensure => directory})
+  create_resources('mount', hiera_hash('mounts', {}), hiera_hash('mount_defaults', {}))
+
+  # hiera vcsrepos
+  create_resources('vcsrepo', hiera_hash('vcsrepos', {}))
 
   # hiera packages
   ensure_packages(hiera_array('packages', []))
